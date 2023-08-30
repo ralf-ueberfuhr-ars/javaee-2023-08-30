4d5e2794a76ee3417d2bc579b57c67eebff12312
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-08-30 14:20:31 +0200

    first commit

---
+++ backup/HelloController.java
@@ -0,0 +1,18 @@
+package de.samples.javaee.todos;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ */
+@Path("/hello")
+@Singleton
+public class HelloController {
+
+    @GET
+    public String sayHello() {
+        return "Hello Team";
+    }
+}
---
+++ src/main/java/de/samples/javaee/todos/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package de.samples.javaee.todos;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // REQ auslesen (Parameter, Header, ...)
+        String name = req.getParameter("name");
+        // auf NULL prüfen
+
+        resp.setContentType("text/html");
+        try(PrintWriter out = resp.getWriter()) {
+            out.println("<h1>Hello</h1><p>Hello " + name + "</p>");
+        }
+    }
+}
+
