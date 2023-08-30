0f7d07cd94b19cfcf77470e965b1e25e71686db6
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2023-08-30 14:34:28 +0200

    Ergänze Aufgabe zu ReadAllTodosServlet

---
+++ src/main/java/de/samples/javaee/todos/ReadAllTodosServlet.java
@@ -0,0 +1,19 @@
+package de.samples.javaee.todos;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ReadAllTodosServlet {
+
+    // private Collection<Todo> todos = Arrays.asList(
+    //  new Todo("Servlets lernen")
+    // );
+
+    // GET /read-todos
+    // -> Liste von Todos (Titel, Beschreibung, Frist)
+
+    // Zusatz:
+    // GET /read-todos?search=lernen
+    // -> Liste von Todos mit "lernen" im Titel
+
+}
